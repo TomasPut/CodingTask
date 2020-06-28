7d358ff44157802e5ec161eb65131ea3de66c2c5
Tomas Putramentas <tomas.putramentas@gmail.com>
2020-06-28 12:38:25 +0300

    First Commit

---
+++ DanskeTask/src/lt/puttomas/codingtask/TestCode.java
@@ -0,0 +1,47 @@
+package lt.puttomas.codingtask;
+
+import java.util.Scanner;
+
+public class TestCode {
+
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter the length of the array: ");
+		int length = s.nextInt();
+		int[] array = new int[length];
+		System.out.print("Enter the elements of the array: "); //after each number hit Enter
+
+		for (int i = 0; i < length; i++) {
+			array[i] = s.nextInt();
+		}
+
+//		int arr[] = { 1, 2, 0, 3, 0, 2, 0 };
+		int n = array.length;
+		System.out.print("Minimum number of jumps to reach end is " + minJumps(array, 0, n - 1));
+	}
+
+	public static int minJumps(int arr[], int a, int b) {
+		int min = Integer.MAX_VALUE;
+
+		// Base case: when source and destination are same 
+		if (b == a) {
+			return 0;
+		}
+        // When nothing is reachable from the given source 
+		if (arr[a] == 0) {
+			return Integer.MAX_VALUE;
+		}
+		/*
+		 * Travels through all the points reachable from array[a]. 
+		 * Recursively get the minimum number of jumps needed to reach 
+		 * array[b] from these reachable points. 
+		 */
+		for (int i = a + 1; i <= b && i <= a + arr[a]; i++) {
+			int jumps = minJumps(arr, i, b);
+			if (jumps != Integer.MAX_VALUE && jumps + 1 < min) {
+				min = jumps + 1;
+			}
+		}
+		return min;
+	}
+}
